669eb567235b9e77868129114b2ac3e375a119c8
marcis abols <marcis.abols@rentokil-initial.com>
2025-07-08 09:40:05 +0300

    TM-3; added angular material

--- FirstApp/src/app/app.ts
+++ FirstApp/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,BrowserAnimationsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
